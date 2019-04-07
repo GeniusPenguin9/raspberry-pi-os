b57b711be8f954340731de153acff87a8f2ba680
penguin <emily@caulyemily.info>
2019-04-07 22:02:51 +0800

    complete exercise 1.3

--- my_exercises/lesson01/include/utils.h
+++ my_exercises/lesson01/include/utils.h
@@ -1,8 +1,12 @@
 #ifndef	_BOOT_H
 #define	_BOOT_H
 
-extern void delay ( unsigned long);
 extern void put32 ( unsigned long, unsigned int );
 extern unsigned int get32 ( unsigned long );
+extern unsigned int proc_id  ( void );
+
+#define delay(i){                   \
+	{for (int j = 0; j < i; j++);}  \
+}
 
 #endif  /*_BOOT_H */
--- my_exercises/lesson01/src/kernel.c
+++ my_exercises/lesson01/src/kernel.c
@@ -1,11 +1,27 @@
 #include "uart.h"
+#include "utils.h"
+
+short init_complete_flag = 0;
+
+
+
+void function(unsigned long cpu)	//processor_id -> x0 -> cpu
+{
+	delay(2000000*cpu);
+
+	uart_send_string("Hello, from processor <");
+	uart_send('0' + cpu);
+	uart_send_string(">\r\n");
+
+	while (1) { }
+}
 
 void kernel_main(void)
 {
 	uart_init();
-	uart_send_string("Hello, world!\r\n");
 
-	while (1) {
-		uart_send(uart_recv());
-	}
+	uart_send_string("UART inited. \r\n");
+
+	function(0);
 }
+
